8ac4afc5b4a0bbe3b071eccf60401333247fea36
Jared Antoine <jared.z.antoine@student.uncommonhs.org>
2018-01-11 22:44:51 +0000

    RPS game

--- js/script.js
+++ js/script.js
@@ -28,6 +28,56 @@
 
 // DOCUMENT READY FUNCTION
 
-var choices = ["rock","paper","scissors"];
-var computerchoice = [];
+var userChoice = ("Rock, Paper or Scissors?");
+
+
+var compare = function(choice1,choice2){
+    if(choice1===choice2){
+        return "The result is a tie!";
+    }
+    if(choice1==="rock"){
+        if(choice2==="scissors"){
+            return "Rock wins";
+        }
+        else{
+            return "Paper wins";
+        }
+    }
+    if(choice1==="paper"){
+        if(choice2==="rock"){
+            return "Paper wins";
+        }
+        else{
+            return "Scissors wins";
+        }
+    }
+    if(choice1==="scissors"){
+        if(choice2==="rock"){
+            return "Rock wins";
+        }
+        else{
+            return "Scissors wins";
+        }
+    }
+};
+$(document).ready(function() {
+        $("#Submit").click(function(){
+                var message = $("#game").val();
+                var computerChoice = Math.random();
+                if (computerChoice <0.34){
+                        computerChoice = "rock";
+                }else if(computerChoice <=0.67){
+                        computerChoice = "paper";
+                }else{
+                        computerChoice = "scissors";
+                }
+                $("#result").text(compare(message,computerChoice));
+        });        
+})
+
+
+
+
+
+
 
